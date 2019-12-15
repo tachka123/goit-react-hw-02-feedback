51abd1808938bdeb74f90d26db23229c0feb2837
Sasha Savchuk <Sasha>
2019-12-15 21:14:45 +0200

    Updates

---
+++ precache-manifest.2dd2d35821be2e69acc66f5bdf3e4dc9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be8d0234132887233f77828085c60e3d",
+    "url": "/goit-react-hw-02-feedback/index.html"
+  },
+  {
+    "revision": "2698d37fe69b4580ca08",
+    "url": "/goit-react-hw-02-feedback/static/css/main.f5626d4f.chunk.css"
+  },
+  {
+    "revision": "e41483ee07f83c1ccbb8",
+    "url": "/goit-react-hw-02-feedback/static/js/2.a7c78240.chunk.js"
+  },
+  {
+    "revision": "2698d37fe69b4580ca08",
+    "url": "/goit-react-hw-02-feedback/static/js/main.f658b728.chunk.js"
+  },
+  {
+    "revision": "4fe7ba823250c0e26226",
+    "url": "/goit-react-hw-02-feedback/static/js/runtime-main.6df1f354.js"
+  }
+]);
